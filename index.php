d081bcb516028c7011f61ff41a3387c14074feaf
nrlhuda67 <81946346+nrlhuda67@users.noreply.github.com>
2021-10-11 12:56:47 +0800

    array

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 09</title>
+    <style>
+        *{
+            font-family: Verdana,Geneva,Tahoma, san-serif;
+        }
+     </style>
+</head>
+<body>
+<?php require 'menu.php'; ?>
+    <h1>Product </h1>
+
+    <?php
+    $air = [
+        'nama' => 'Drinking Water ',
+        'size' => '500 ml',
+        'harga' => '70',
+    ];
+    ?>
+
+    <table>
+        <tr>
+            <td>Nama : </td>
+            <td><?php echo $air['nama']; ?></td>
+        </tr>
+
+        <tr>
+            <td>Size : </td>
+            <td><?php echo $air['size']; ?></td>
+        </tr>
+
+        <tr>
+            <td>Harga : </td>
+            <td><?php echo $air['harga']; ?> sen </td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,6 @@
+<h1> LATIHAN </h1>
+[
+    <a href="index.php">Product</a>
+    |
+    <a href="trans.php">Transaction</a>
+    ]
